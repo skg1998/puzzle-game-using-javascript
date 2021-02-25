aa9858bad752eae309694ea37acb37da67f4adca
Sahil Gupta <49576577+skg1998@users.noreply.github.com>
2021-02-25 12:40:24 +0530

    Update dummy.js

--- dummy.js
+++ dummy.js
@@ -5,5 +5,6 @@ for(let i=0;i<=9;i++)
 {
 document.getElementsByClassName("pic")[i].setAttribute("src",randompic);
 random=Math.floor(Math.random()*9+1);
+  console.log(random);
 randompic=`${random}.jpg`;
 }
